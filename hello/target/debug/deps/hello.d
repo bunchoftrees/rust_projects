f59912cf8d0ceceab6d66e149652d821179349ef
Forrest JB Hartley <forrest@forrestbear.org>
2021-08-13 21:11:24 -0400

    add hello/ debug log

---
+++ hello/target/debug/deps/hello.d
@@ -0,0 +1,5 @@
+/Users/forrest/rust_projects/hello/target/debug/deps/hello: src/main.rs
+
+/Users/forrest/rust_projects/hello/target/debug/deps/hello.d: src/main.rs
+
+src/main.rs:
---
+++ hello/target/debug/hello.d
@@ -0,0 +1,1 @@
+/Users/forrest/rust_projects/hello/target/debug/hello: /Users/forrest/rust_projects/hello/src/main.rs
